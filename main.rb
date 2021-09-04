0057cb1271e25254288086c7606b497a075b87a0
‘kuma’ <km923.hr811@gmail.com>
2021-09-04 21:19:26 +0900

    p

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
-p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-p "じゃんけんポン！"
+def pon
+  p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  p "じゃんけんポン！"
+end
+pon
 
 def janken
   p "[0]:グー✊ [1]:チョキ🤘 [2]:パー✋ [3]:戦わない🚨"
@@ -13,7 +16,7 @@ def janken
 
   if player == program
     p "あいこでショ！"
-    return true
+    #return trueでnexr_gameに
   elsif (player == 0 && program == 1) || (player == 1 && program == 2) || (player == 2 && program == 0)
     p "勝ちーー！"
     return acchihoi_winer
@@ -38,34 +41,45 @@ def acchihoi_winer
 
   p "あなたの手は#{hands[player]}, 相手は#{hands[program]}を向きました"
   p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  if player == 0 || player == 1 || player == 2 || player == 3
-    p "じゃんけんポン！"
-   return janken
-  else
+
+  if player < 0 || 4 <= player
     p "終わりです。お疲れ様です。"
-    return  
+    return
+  elsif player == program
+    p "勝ちーー！！やったー！"
+    pon
+    #return trueでnexr_gameに
+  else
+    p "ダメでした！もう一度じゃんけんから！"
+    pon
+    #return trueでnexr_gameに
   end
 end
 
 def acchihoi_loser
-    p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    p "あっちむいてーーホイ！"
-    p "[0]:👆 [1]:👉 [2]:👇 [3]:👈 [4]:戦わない🚨"
+  p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  p "あっちむいてーーホイ！"
+  p "[0]:👆 [1]:👉 [2]:👇 [3]:👈 [4]:戦わない🚨"
   
-    player = gets.to_i 
-    program = rand(4)
+  player = gets.to_i 
+  program = rand(4)
   
-    hands = ["上", "右", "下", "左", "棄権"]
+  hands = ["上", "右", "下", "左", "棄権"]
   
-    p "相手の手は#{hands[program]}, あなたは#{hands[player]}を向きました。"
-    p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    if player == 0 || player == 1 || player == 2 || player == 3
-      p "じゃんけんポン！"
-     return janken
-    else
-      p "終わりです。お疲れ様です。"
-      return  
-    end
+  p "相手の手は#{hands[program]}, あなたは#{hands[player]}を向きました。"
+  p "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  if player < 0 || player >= 4
+    p "終わりです。お疲れ様です。"
+    return
+  elsif player == program
+    p "負けーー！！残念！"
+    pon
+    #return trueでnexr_gameに
+  else
+    p "セーフ！もう一度じゃんけんから！"
+    pon
+    #return trueでnexr_gameに
+  end
 end
 
 next_game = true
@@ -73,3 +87,7 @@ next_game = true
 while next_game do
     next_game = janken
 end
+
+
+
+
